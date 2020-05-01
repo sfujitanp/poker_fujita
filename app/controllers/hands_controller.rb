4f817292e06c77dd4b7d64934fc1ba526520aeab
sfujitanp <sfujita@netprotections.co.jp>
2020-05-01 17:02:41 +0900

    input情報をスートを数字に分けて格納する

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -1,10 +1,27 @@
 class HandsController < ApplicationController
   def rule
-   #cards = @input.split(" ")
-   #flash['notice'] = cards
-   input = params[:content]
-   flash[:notice] = input
 
+   #画面入力情報の受け取り
+   input = params[:content]
+   cards = input.split
+   i = 0
+   suit = []
+   number = []
+
+   puts suit
+
+   #スートと数字に分けて格納
+   cards.each do|card|
+    suit[i] = card.scan(/[a-z]/)
+    number[i] = card.scan(/[^a-z]/)
+    i += 1
+   end
+
+   #役判定ロジック
+
+
+   #画面に結果を表示
+   flash[:notice] = suit + number
    render("home/top")
 
   end
