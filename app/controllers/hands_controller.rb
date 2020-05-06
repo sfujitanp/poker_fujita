2d31a7209b6766e9121cba7564ea7b67ab7049e8
sfujitanp <sfujita@netprotections.co.jp>
2020-05-06 09:41:38 +0900

    連速した数字かどうか判定する

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -1,35 +1,37 @@
 class HandsController < ApplicationController
   def rule
 
-   #画面入力情報の受け取り
-   input = params[:content]
-   cards = input.split
-   i = 0
-   suits = []
-   numbers = []
+   #入力情報をスートと数字に分けて格納
+    input = params[:content]
+    suits = input.scan(/[a-zA-Z]/)
+    numbers = input.scan(/\d/).sort
+     #TODO このタイミングで、整数に変換したい
+
+   #数字の差分を格納
+    diff = []
+    (0..3).each do |j|
+      diff[j] = (numbers[j+1].to_i - numbers[j].to_i)
+    end
+
+   #判定結果の格納先を定義
+   ranks = {same_suits:0,serial_number:0,same_number:0}
 
 
-   #スートと数字に分けて格納
-   cards.each do|card|
-    suits[i] = card.scan(/[a-z]/)
-    numbers[i] = card.scan(/[^a-z]/)
-    i += 1
-   end
-
-   #役判定ロジック
-   #判定結果をranksに格納していく
-   ranks = []
-
+   ###役判定ロジック###
    #①全部同じスートかどうか
    if suits.uniq.count == 1 then
-     ranks[0] = 1
-   elsif
-      ranks[0] = 0
+     ranks[:same_suites] = 1
    end
 
-
    #②５枚が連続した数字か
+   if diff == [1,1,1,1] || diff.sort == [1,1,1,9] then
+     ranks[:serial_number] = 1
+   end
 
+   #③５枚が同じ数字が何個あるか
+    if diff == [0,0,0,0] then
+      ranks[:same_number] = 1
+    end
 
    #画面に結果を表示
    flash[:notice] = ranks
