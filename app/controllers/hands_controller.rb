1ae83f686e8757009aefbe3dc7412920e47c3a37
sfujitanp <sfujita@netprotections.co.jp>
2020-05-12 20:42:39 +0900

    cardクラスを定義し、手札に対してインスタンスを作成するようにした

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -1,18 +1,24 @@
 class HandsController < ApplicationController
   include Judge
 
-  #入力情報を格納
-  def input
-    params[:content]
-  end
-
   #バリデーションを確認し、結果を返却する
   def rule
-    if  valid? then
-       flash[:notice] = judge
+    card = Card.new(input)
+
+    if  card.valid? then
+       flash[:notice] = card.judge
     else
        flash[:notice] = "5つのカード指定文字を半角スペース区切りで入力してください。（例：S1 H3 D9 C13 S11）"
     end
    render("home/top")
   end
+
+  private
+
+  #入力情報を格納
+  def input
+    params[:content]
+  end
+
 end
+
