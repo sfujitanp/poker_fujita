de5daf9c4447afb833e9725578e9aae108697634
sfujitanp <sfujita@netprotections.co.jp>
2020-05-18 21:47:27 +0900

    serviceのテストを作成した

--- spec/controllers/hands_controller_spec.rb
+++ spec/controllers/hands_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe HandsController, type: :controller do
     before do
       get :top
     end
+
     it '200レスポンスを返す' do
       expect(response.status).to eq 200
     end
@@ -20,7 +21,8 @@ RSpec.describe HandsController, type: :controller do
     before do
       post :rule
     end
-    let(:card_ok) {Card.new("S3 S4 S5 S6 S7")}
+
+    let(:card_ok) {Card.new('S3 S4 S5 S6 S7')}
     let(:card_ng) {Card.new('S3 S4 S5 S6')}
     it '200レスポンスを返す' do
       expect(response.status).to eq 200
@@ -28,23 +30,28 @@ RSpec.describe HandsController, type: :controller do
     it 'cardインスタンスを作成できている' do
       expect(card_ok).to be_valid
     end
+
     context 'バリデーションがOK' do
       it 'バリデーションがOKとなる' do
         expect(card_ok.valid?).to eq true
       end
-
       it 'ランクの結果が返却される' do
       expect(card_ok.rank_judge).to eq "ストレートフラッシュ"
       end
     end
+
     context 'バリデーションがNG' do
       it 'バリデーションがNGとなる' do
         expect(card_ng.valid?).to eq false
       end
     end
+
+    context 'hoge' do
+    #let(:input){'S3 S4 S5 S6 S7'}
     it 'top画面で入力した内容をruleアクションでinputとして受け取っている' do
-      #実装方法不明
+    #　実装方法を相談したい
+    #  expect(flash[:notice]).to eq "ストレートフラッシュ"
+    end
     end
    end
-
 end
--- spec/services/judge_spec.rb
+++ spec/services/judge_spec.rb
@@ -5,82 +5,90 @@ describe 'バリデーション' do
 
   it '【OK】５つのカードで、全部一桁' do
     card = Card.new('S3 S4 S5 S6 S7')
-    expect(card).to be_valid
+    expect(card.valid?).to eq true
   end
 
   it '【OK】５つのカードで２桁の数字を含む' do
-
+    card = Card.new('S10 S11 S12 S13 S1')
+    expect(card.valid?).to eq true
   end
 
   it '【NG】カードが５枚ではない' do
-
+    card = Card.new('S3 S4 S5 S6')
+    expect(card.valid?).to eq false
   end
 
   it '【NG】区切りが半角スペースではない' do
-
+    card = Card.new('S3　S4　S5　S6　S7')
+    expect(card.valid?).to eq false
   end
 
   it '【NG】指定以外のスート' do
-
+    card = Card.new('A10 S11 S12 S13 S1')
+    expect(card.valid?).to eq false
   end
 
   it '【NG】指定以外の数字' do
-
+    card = Card.new('S0 S14 S12 S13 S1')
+    expect(card.valid?).to eq false
   end
 
   it '【NG】無関係の文字' do
-
+    card = Card.new('hoge')
+    expect(card.valid?).to eq false
   end
 
   it '【NG】同じカードを含む' do
-
+    card = Card.new('S11 S11 S12 S13 S1')
+    expect(card.valid?).to eq false
   end
-
-  it 'valid?の結果がflashに引き渡されている' do
-
-  end
-
-
 end
 
 
 describe '役判定' do
   it 'ストレートフラッシュ' do
-
+    card = Card.new('S10 S11 S12 S13 S1')
+    expect(card.rank_judge).to eq "ストレートフラッシュ"
   end
 
   it 'フォー・オブ・ア・カインド' do
-
+    card = Card.new('C10 D10 H10 S10 D5')
+    expect(card.rank_judge).to eq "フォー・オブ・ア・カインド"
   end
 
   it 'フルハウス' do
-
+    card = Card.new('S10 H10 D10 S4 D4')
+    expect(card.rank_judge).to eq "フルハウス"
   end
-  it 'フラッシュ' do
 
+  it 'フラッシュ' do
+    card = Card.new('H1 H12 H10 H5 H3')
+    expect(card.rank_judge).to eq "フラッシュ"
   end
 
   it 'ストレート' do
-
+    card = Card.new('S8 S7 H6 H5 S4')
+    expect(card.rank_judge).to eq "ストレート"
   end
-  it 'スリー・オブ・ア・カインド' do
 
+  it 'スリー・オブ・ア・カインド' do
+    card = Card.new('S12 C12 D12 S5 C3')
+    expect(card.rank_judge).to eq "スリー・オブ・ア・カインド"
   end
 
   it 'ツーペア' do
-
+    card = Card.new('H13 D13 C2 D2 H11')
+    expect(card.rank_judge).to eq "ツーペア"
   end
 
   it 'ワンペア' do
-
+    card = Card.new('C10 S10 S6 H4 H2')
+    expect(card.rank_judge).to eq "ワンペア"
   end
 
   it 'ハイカード' do
-
-  end
-
-  it '役判定結果がflashに引き渡されている' do
-
+    card = Card.new('D1 D10 S9 C5 C4')
+    expect(card.rank_judge).to eq "ハイカード"
   end
 
 end
