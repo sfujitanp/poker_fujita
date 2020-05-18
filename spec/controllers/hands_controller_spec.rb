6c6d93b6ab1f4bbe078b82db523a4d76f23ba39e
sfujitanp <sfujita@netprotections.co.jp>
2020-05-18 23:50:38 +0900

    controller #ruluのテストにtopの描画を追加

--- spec/controllers/hands_controller_spec.rb
+++ spec/controllers/hands_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe HandsController, type: :controller do
     before do
       get :top
     end
-
     it '200レスポンスを返す' do
       expect(response.status).to eq 200
     end
@@ -21,7 +20,6 @@ RSpec.describe HandsController, type: :controller do
     before do
       post :rule
     end
-
     let(:card_ok) {Card.new('S3 S4 S5 S6 S7')}
     let(:card_ng) {Card.new('S3 S4 S5 S6')}
     it '200レスポンスを返す' do
@@ -29,6 +27,9 @@ RSpec.describe HandsController, type: :controller do
     end
     it 'cardインスタンスを作成できている' do
       expect(card_ok).to be_valid
+    end
+    it 'topビューが描画されている' do
+      expect(response).to render_template(:top)
     end
 
     context 'バリデーションがOK' do
@@ -53,5 +54,6 @@ RSpec.describe HandsController, type: :controller do
     #  expect(flash[:notice]).to eq "ストレートフラッシュ"
     end
     end
+
    end
 end
