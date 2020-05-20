fb9668ee2d9e8f499669751534c74960cf9335b7
sfujitanp <sfujita@netprotections.co.jp>
2020-05-20 13:50:00 +0900

    POST #judge with params　のテストを作成

--- spec/controllers/hands_controller_spec.rb
+++ spec/controllers/hands_controller_spec.rb
@@ -18,17 +18,10 @@ RSpec.describe HandsController, type: :controller do
 
   describe "POST #judge" do
     before do
-      post :judge, params: 'S3 S4 S5 S6 S7'
+      post :judge
     end
     let(:card_ok) {Card.new('S3 S4 S5 S6 S7')}
     let(:card_ng) {Card.new('S3 S4 S5 S6')}
-
-    it 'cardインスタンスを作成できている' do
-      expect(card_ok).to be_valid
-    end
-    it 'topビューが描画されている' do
-      expect(response).to render_template(:top)
-    end
 
     context 'バリデーションがOK' do
       it 'バリデーションがOKとなる' do
@@ -38,19 +31,31 @@ RSpec.describe HandsController, type: :controller do
         card_ok.judge_rank
         expect(card_ok.rank_name).to eq "ストレートフラッシュ"
       end
-      it '200レスポンスを返す' do
-        expect(response.status).to eq 200
-      end
     end
 
     context 'バリデーションがNG' do
       it 'バリデーションがNGとなる' do
         expect(card_ng.valid?).to eq false
       end
-      it '400レスポンスを返す' do
-        expect(response.status).to eq 400
-      end
     end
+  end
 
-   end
+  describe "POST #judge with params" do
+    it '400レスポンスを返す' do
+      post :judge, params: { content: " "}
+      expect(response.status).to eq 400
+    end
+    it 'topビューが描画されている' do
+      post :judge, params: { content: " "}
+      expect(response).to render_template(:top)
+    end
+    it '200レスポンスを返す' do
+      post :judge, params: { content: "S3 S4 S5 S6 S7"}
+      expect(response.status).to eq 200
+    end
+    it 'topビューが描画されている' do
+      post :judge, params: { content: "S3 S4 S5 S6 S7"}
+      expect(response).to render_template(:top)
+    end
+  end
 end
